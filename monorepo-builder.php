6a6b51ed0d1de4957bbcff35013d3f3473f1e9bc
NobodyExpectsTheSpanishInquisition <s.miklaszewski1999@gmail.com>
2021-08-19 22:38:54 +0200

    create monorepo

---
+++ monorepo-builder.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use MonorepoBuilder20210818\Symplify\ComposerJsonManipulator\ValueObject\ComposerJsonSection;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\MonorepoBuilder\ValueObject\Option;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+
+    $parameters->set(Option::PACKAGE_DIRECTORIES, [
+        __DIR__ . '/packages/php'
+    ]);
+
+    // for "merge" command
+    $parameters->set(Option::DATA_TO_APPEND, [
+        ComposerJsonSection::REQUIRE_DEV => [
+            'phpunit/phpunit' => '^9.5',
+        ],
+    ]);
+};
